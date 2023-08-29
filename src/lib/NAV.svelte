abf003c4cba9ae10444df70c22d2ca4408125b0b
Taylah Baillie (1000054880) <BAILTJ1@student.op.ac.nz>
2023-08-30 09:38:36 +1200

    buggering around with header/nav

--- src/lib/NAV.svelte
+++ src/lib/NAV.svelte
@@ -1,18 +1,25 @@
 <style>
     nav {
         width :100%;
-        background-color: pink;
-        padding: 2rem 4rem;
+        background-color: lightgray;
+        padding: 4rem 4rem;
+    }
+    nav #upper {
+        display: flex;
+        flex-direction: row;   
+        justify-content: center;
     }
 
     nav #navb {
         display: flex;
         flex-direction: row;
         justify-content: center;
+ 
     }
 
     nav a {
-        padding: 0 4rem;
+        padding: 0 9rem;
+        font-size: 1.75rem;
         text-decoration: none;
         color: black;
         font-family: 'Montserrat', sans-serif;
@@ -21,10 +28,13 @@
 </style>
 
 <nav>
+    <div id ="upper">
+        <a href="https://online.op.ac.nz/"> Adoption and Co</a>
+    </div>
     <div id = "navb">
         <a href = "https://online.op.ac.nz/"> Home </a>
-        <a href = "https://online.op.ac.nz/"> Page 1 </a>
-        <a href = "https://online.op.ac.nz/"> Page 2 </a>
-        <a href = "https://online.op.ac.nz/"> Page 3 </a>
+        <a href = "https://online.op.ac.nz/"> Dogs </a>
+        <a href = "https://online.op.ac.nz/"> Cats </a>
+        <a href = "https://online.op.ac.nz/"> Donate </a>
     </div>
 </nav>
