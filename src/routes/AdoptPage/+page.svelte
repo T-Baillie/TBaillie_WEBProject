c0e3f8feaf867e7b333cb48303780910b06efcef
Taylah Baillie (1000054880) <BAILTJ1@student.op.ac.nz>
2023-10-27 11:22:57 +1300

    made adopt imgs, now have borders and padding

--- src/routes/AdoptPage/+page.svelte
+++ src/routes/AdoptPage/+page.svelte
@@ -5,7 +5,7 @@
 
   onMount(async () => {
     const res = await fetch(`
-    ${BASE_URL}/search/photos?query=pets&per_page=12&color=blue&orientation=portrait&client_id=7EFeIv8qjanpMATe8ge5S-Bt6PBIeyUYwzcblESBly4
+    ${BASE_URL}/search/photos?query=pets&per_page=12&orientation=portrait&client_id=7EFeIv8qjanpMATe8ge5S-Bt6PBIeyUYwzcblESBly4
     `);
     let data = await res.json();
     imgs = data.results;
