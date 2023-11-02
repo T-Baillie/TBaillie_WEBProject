4015cd2a013a094293b70633db72682f66d2b8e1
Taylah Baillie (1000054880) <BAILTJ1@student.op.ac.nz>
2023-11-03 11:16:58 +1300

    cat fact button now works, removed unneeded code

--- src/routes/OtherPage/+page.svelte
+++ src/routes/OtherPage/+page.svelte
@@ -1,36 +1,25 @@
 <script>
-  import { onMount } from "svelte";
   let count = 1;
   let fact = "";
-  
+
   const BASEURLFacts = "https://meowfacts.herokuapp.com";
 
-  onMount(async () => {
+  const api = async () => {
     const res = await fetch(`${BASEURLFacts}/?count=${count}`);
     const data = await res.json();
     fact = data.data;
     console.log(fact);
-    console.log(count);
-  });
+  };
 
-  function factprint() {
-    count += 1;
-    console.log(count);
-    onMount(async () => {
-      const res = await fetch(`${BASEURLFacts}/?count=${count}`);
-      const data = await res.json();
-      fact = data.data;
-      console.log(fact);
-    });
-  }
 </script>
 
 <div class="button">
-  <button class="fact_button" on:click={factprint}>
-    Press for cat fact
-  </button>
+  <button class="fact_button" on:click={api}> Press for cat fact </button>
 </div>
 <div>
-  {fact}
+  {#each fact as facts}
+    <p>
+      {facts}
+    </p>
+  {/each}
 </div>
-
