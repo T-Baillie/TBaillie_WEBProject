b287f34ed2b7fc5da1cc889597b111ddd1cc6ba5
Taylah Baillie (1000054880) <BAILTJ1@student.op.ac.nz>
2023-11-07 11:53:47 +1300

    begun work on footer

--- src/lib/FOOTER.svelte
+++ src/lib/FOOTER.svelte
@@ -1,3 +1,9 @@
-<footer class ="footer">
-
+<footer class="footer">
+  <div class="foot-title">
+    <h1>Adoption and Co</h1>
+    <ul class="foot-content">Home</ul>
+    <ul class="foot-content">adopt</ul>
+    <ul class="foot-content">Donate</ul>
+    <ul class="foot-content">Other</ul>
+  </div>
 </footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
-  import Header from '$lib/HEADER.svelte';
-  import Footer from '$lib/FOOTER.svelte';
+  import Header from "$lib/HEADER.svelte";
+  import Footer from "$lib/FOOTER.svelte";
 </script>
 
 <svelte:head>
-    <link rel = "stylesheet" href = "css/nav.css">
-    <link rel = "stylesheet" href= "css/main.css">
-    <link rel = "stylesheet" href = "css/adopt.css">
-    <link rel = "stylesheet" href = "css/other.css">
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/footer.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/adopt.css" />
+  <link rel="stylesheet" href="css/other.css" />
 </svelte:head>
 
 <Header />
